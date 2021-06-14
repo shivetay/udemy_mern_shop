af1a7fd144cee5c79b0d5810e42b1befddef12ae
Łukasz Dawidowicz <lddev@outlook.com>
2021-06-14 13:09:37 +0200

    add: seeder data script

---
+++ data/users.js
@@ -0,0 +1,20 @@
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: 'test1234',
+    isAdmin: true,
+  },
+  {
+    name: 'Mieszko',
+    email: 'mieszko@mail.com',
+    password: 'test1234',
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: 'test1234',
+  },
+];
+
+export default users;
