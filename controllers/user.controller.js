d5bce1d65e5ea2cc9f6a98e90e3218b63d0f5c4f
Łukasz Dawidowicz <lddev@outlook.com>
2021-06-23 13:01:15 +0200

    add: user return after auth

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -35,7 +35,18 @@ const authUser = asyncHandler(async (req, res) => {
 @access private
 */
 const getProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById();
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+  }
 });
 
 export { authUser, getProfile };
